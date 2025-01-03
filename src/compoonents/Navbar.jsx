a84f56229a9006482f095fc7eaadf59df5290aa7
SarMSA <saramahmoud5797@gmail.com>
2025-01-03 13:41:55 +0200

    last commit

--- src/compoonents/Navbar.jsx
+++ src/compoonents/Navbar.jsx
@@ -1,16 +1,53 @@
+import { useGSAP } from "@gsap/react";
 import { navLists } from "../constants";
-import { appleImg, bagImg, searchImg } from "../utils";
-
+import { appleImg, bagImg, menuIconImg, searchImg } from "../utils";
+import { useState, useEffect } from "react";
+import gsap from "gsap";
 const Navbar = () => {
+  const [active, setActive] = useState("open");
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+      if (window.innerWidth > 640) {
+        setActive("open");
+      } else {
+        setActive("close");
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useGSAP(() => {
+    if (active === "open") {
+      gsap.to(".menuIcon", {
+        y: 0,
+        duration: 0.5,
+        ease: "power2.inOut",
+        opacity: 1,
+      });
+    } else {
+      gsap.to(".menuIcon", {
+        y: 100,
+        duration: 0.5,
+        ease: "power2.inOut",
+        opacity: 0,
+      });
+    }
+  }, [active]);
   return (
-    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
-      <nav className="flex justify-between w-full align-baseline screen-max-width ">
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-baseline relative">
+      <nav className="flex justify-between w-full items-center screen-max-width ">
         <img src={appleImg} alt="Apple" width={14} height={18} />
-        <div className="max-sm:hidden flex flex-1 justify-center">
+
+        <div className={`flex flex-1 justify-center menuIcon `}>
           {navLists.map((nav) => (
             <div
               key={nav}
-              className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all"
+              className={`px-5 text-sm cursor-pointer text-gray hover:text-white transition-all `}
             >
               {nav}
             </div>
@@ -20,6 +57,18 @@ const Navbar = () => {
           <img src={searchImg} alt="search" width={18} />
           <img src={bagImg} alt="bag" width={18} />
         </div>
+        {windowWidth < 640 ? (
+          <img
+            src={menuIconImg}
+            alt="menu"
+            className="w-[50px]"
+            onClick={() => {
+              active === "close" ? setActive("open") : setActive("close");
+            }}
+          />
+        ) : (
+          ""
+        )}
       </nav>
     </header>
   );
